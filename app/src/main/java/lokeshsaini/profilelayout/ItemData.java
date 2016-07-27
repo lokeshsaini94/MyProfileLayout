b99ea4d83ecbcbd917bc784fc76cd9f6efd13471
lokeshsaini94 <lokeshsaini94@gmail.com>
2016-07-27 23:13:33 +0530

    Added Firebase User Authentication and user profile

---
+++ app/src/main/java/lokeshsaini/profilelayout/ItemData.java
@@ -0,0 +1,20 @@
+package lokeshsaini.profilelayout;
+
+
+public class ItemData {
+    private String mItemText;
+
+    public ItemData() {}
+
+    public ItemData(String mItemText) {
+        this.mItemText = mItemText;
+    }
+
+    public String getmItemText() {
+        return mItemText;
+    }
+
+    public void setmItemText(String mItemText) {
+        this.mItemText = mItemText;
+    }
+}
--- app/src/main/java/lokeshsaini/profilelayout/TabFragment.java
+++ app/src/main/java/lokeshsaini/profilelayout/TabFragment.java
@@ -3,39 +3,72 @@ package lokeshsaini.profilelayout;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import java.util.ArrayList;
+import android.widget.Toast;
 
 
 public class TabFragment extends Fragment {
-    ArrayList<Item> items;
+    String itemName;
+    int itemCounter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_tab, container, false);
-        // Lookup the recyclerview in activity layout
-        RecyclerView rvItems = (RecyclerView) root.findViewById(R.id.rvItems);
 
-        // Initialize items
-        items = Item.createItemsList(10);
-        // Create adapter passing in the sample user data
-        ItemsAdapter adapter = new ItemsAdapter(getContext(), items);
+        itemName = "Item ";
+        itemCounter = 1;
 
-//        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-//        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-//        rvItems.setLayoutManager(layoutManager);
+        // 1. get a reference to recyclerView
+        final RecyclerView recyclerView = (RecyclerView) root.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
-        // Attach the adapter to the recyclerview to populate items
-        rvItems.setAdapter(adapter);
-        // Set layout manager to position the items
-        rvItems.setLayoutManager(new LinearLayoutManager(getActivity()));
+        // this is data for recycler view
+        final ItemData itemsData[] = {
+                new ItemData(itemName + itemCounter++),
+                new ItemData(itemName + itemCounter++),
+                new ItemData(itemName + itemCounter++),
+                new ItemData(itemName + itemCounter++),
+                new ItemData(itemName + itemCounter++),
+                new ItemData(itemName + itemCounter++),
+                new ItemData(itemName + itemCounter++),
+                new ItemData(itemName + itemCounter++),
+                new ItemData(itemName + itemCounter++),
+                new ItemData(itemName + itemCounter++),
+                new ItemData(itemName + itemCounter++),
+                new ItemData(itemName + itemCounter++),
+                new ItemData(itemName + itemCounter++),
+                new ItemData(itemName + itemCounter++),
+                new ItemData(itemName + itemCounter++)
+        };
+
+        // 2. set layoutManger
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        // 3. create an adapter
+        MyAdapter mAdapter = new MyAdapter(itemsData);
+        // 4. set adapter
+        recyclerView.setAdapter(mAdapter);
+        // 5. set item animator to DefaultAnimator
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        // 6. on Item touch listener
+        recyclerView.addOnItemTouchListener(
+                new RecyclerItemClickListener(getContext(), new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override public void onItemClick(View view, int position) {
+                        int x = position + 1;
+                        String item = itemName + x;
+                        Toast.makeText(getActivity(), item,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                })
+        );
 
         // Inflate the layout for this fragment
         return root;
